f077c0cba248816448def6b15f47ca83a0316bc0
Dário Nascimento <dfrnascimento@gmail.com>
2013-05-04 03:39:43 +0100

    ddadasdadada

--- EADW-Political/crawler/commandInterface.py
+++ EADW-Political/crawler/commandInterface.py
@@ -12,8 +12,13 @@ engine.setDBName(dbpath);
 #entities,global entity reputation,news opinion, partido
 def searchNews(search_word):
     result = []
-    for score, link, entities in engine.searchTopWithEntity(search_word, 5):
-        return "Score: "+str(score), "Link: "+link, "Entities: ", entities
+    entities = []
+    entities.append({"entityName":"Dario","entityGlobalReputation":"32","opinion":"1","partido":"PSD"})    
+    return {"link":"http://www.ist.utl.pt","title":"Dario IST","score":"12","summary":"Summary do texto",
+                "globalFeeling":"dario","entities":entities}
+    #for score, link, entities,summary,title in engine.searchTopWithEntity(search_word, 5):
+        #for entity in entitiesList:
+        #return "Score: "+str(score), "Link: "+link, "Entities: ", entities
 
 
 
@@ -26,7 +31,9 @@ def searchNews(search_word):
 #Nº Positivas por dia
 #Nº Negativas por dia
 def getEntityDetails(entity):
-    return "TODO"
+    pos = {"432434":"12","432434":"12","432434":"12","432434":"12","432434":"12","432434":"12"}
+    neg = {"432434":"12","432434":"12","432434":"12","432434":"12","432434":"12","432434":"12"}
+    return {"entity":"Dario","reputation":"123","pos":13,"neg":15,"partido":"PSD","pos":pos,"neg":neg}
 
 #Lista de partidos:
 # -> Numero de noticas
@@ -34,11 +41,17 @@ def getEntityDetails(entity):
 # -> Nº Negativas por dia
 #Depos faz o acumulado
 def partidos():
-    return "TODO"
+    pos = {"432434":"12","432434":"12","432434":"12","432434":"12","432434":"12","432434":"12"}
+    neg = {"432434":"12","432434":"12","432434":"12","432434":"12","432434":"12","432434":"12"}
+    return {"nNoticias":132,"pos":pos,"neg":neg}
 
 #Quais as palavras mais ditas
 def topWords():
-    return "TODO"
+    return {"dario":43,"dara":32,"daasd":12,"dadasda":65,"addadsada":323}
 
 def topCountries():
-    return "TODO"
+    return {"portugal":43,"espanha":32,"turquia":12,"italia":65,"bulgaria":323}
+
+
+
+print searchNews("dario")
